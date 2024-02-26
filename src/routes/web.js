91339172570e8a0f4872ecaacbb1d7da92a7491b
thinhvo18 <thinhvq18112002@gmail.com>
2024-02-26 17:08:11 +0700

    use bootstrap 5

--- src/routes/web.js
+++ src/routes/web.js
@@ -9,6 +9,7 @@ const router = express.Router();
  */
 const initWebRoutes = (app) => {
     router.get('/', homeController.handleHelloWord);
+    router.get('/user', homeController.handleUserPage);
 
     return app.use('/', router);
 };
