f100ffed88a1fc1e9d9ce031f29c47d1910700b6
thinhvo18 <thinhvq18112002@gmail.com>
2024-02-26 16:20:25 +0700

    mvc

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import homeController from '../controller/homeController';
 
 const router = express.Router();
 
@@ -7,9 +8,7 @@ const router = express.Router();
  * @param {*} app - express app
  */
 const initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send('Hello world');
-    });
+    router.get('/', homeController.handleHelloWord);
 
     return app.use('/', router);
 };
