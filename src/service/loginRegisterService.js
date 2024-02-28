f50778b941f2d00e54f0561b18bc557ecb539052
thinhvo18 <thinhvq18112002@gmail.com>
2024-02-28 23:38:22 +0700

    complete login

--- src/service/loginRegisterService.js
+++ src/service/loginRegisterService.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcryptjs';
+import { Op } from 'sequelize';
 import db from '../models';
 
 const salt = bcrypt.genSaltSync(10);
@@ -73,6 +74,44 @@ const registerNewUser = async (rawUserData) => {
     }
 };
 
+const checkPassword = (inputPassword, hashPassword) => {
+    return bcrypt.compareSync(inputPassword, hashPassword);
+};
+
+const loginUser = async (rawData) => {
+    try {
+        let user = await db.User.findOne({
+            where: {
+                [Op.or]: [{ email: rawData.valueLogin }, { phone: rawData.valueLogin }],
+            },
+        });
+
+        if (user) {
+            let isCorrectPassword = checkPassword(rawData.password, user.password);
+
+            if (isCorrectPassword) {
+                return {
+                    EM: 'Successfully!',
+                    EC: 0,
+                    DT: '',
+                };
+            }
+        }
+        return {
+            EM: 'Your email/phone number or password is incorrect!',
+            EC: 1,
+            DT: '',
+        };
+    } catch (error) {
+        console.log(error);
+        return {
+            EM: 'Something is wrong in service...',
+            EC: -2,
+        };
+    }
+};
+
 module.exports = {
     registerNewUser,
+    loginUser,
 };
