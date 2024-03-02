61e4a40d9c7ad1fe442e8b3f6e0a4d984af1c02f
thinhvo18 <thinhvq18112002@gmail.com>
2024-03-02 09:58:32 +0700

    JWT Middleware

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,18 +2,15 @@ import express from 'express';
 import apiController from '../controller/apiController';
 import userController from '../controller/userController';
 import groupController from '../controller/groupController';
+import { checkUserJWT, checkUserPermission } from '../middleware/JWTActions';
 
 const router = express.Router();
 
-/**
- *
- * @param {*} app - express app
- */
 const initApiRoutes = (app) => {
     router.post('/register', apiController.handleRegister);
     router.post('/login', apiController.handleLogin);
 
-    router.get('/user/read', userController.readFunc);
+    router.get('/user/read', checkUserJWT, checkUserPermission, userController.readFunc);
     router.post('/user/create', userController.createFunc);
     router.put('/user/update', userController.updateFunc);
     router.delete('/user/delete', userController.deleteFunc);
